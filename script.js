2714d4f5eca75a041143f30d3c25bf38b0be270f
nukeador <nukeador@protonmail.com>
2023-11-21 22:48:13 +0100

    Fixes para los cambios del api

--- script.js
+++ script.js
@@ -100,8 +100,9 @@ function fetchBusTime(stopNumber, lineNumber, lineItem) {
     fetch(apiUrl)
         .then(function(response) { return response.json(); })
         .then(function(data) {
-            if (data && data.length > 0) {
-                var busInfo = data[0];
+            if (data.buses && data.buses.length > 0) {
+                var paradaInfo = data.parada;
+                var busInfo = data.buses[0];
                 lineItem.innerHTML = '<div class="linea"><h3>' + lineNumber + '</h3><p class="destino">→ ' + busInfo.destino + '</p></div> <div class="tiempo">' + busInfo.tiempoRestante + ' <p>min.</p></div>';
             } else {
                 lineItem.innerHTML = '<div class="linea"><h3>' + lineNumber + '</h3></div> <div class="tiempo">Sin info</div>';;
