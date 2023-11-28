c639a209672e17eaf1fdcaeeb2f055ad772010fa
nukeador <nukeador@protonmail.com>
2023-11-28 14:54:28 +0100

    Control de cache obsoleto

--- service-worker.js
+++ service-worker.js
@@ -52,3 +52,18 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+
+// Elimina los recursos antiguos del cache
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
