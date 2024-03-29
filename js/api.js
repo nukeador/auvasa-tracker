0ed151e1b5e7fcd99ad889673044567df6296ce1
nukeador <nukeador@protonmail.com>
2024-03-29 01:43:23 +0100

    Texto de parada suprimida

--- js/api.js
+++ js/api.js
@@ -649,7 +649,7 @@ export async function updateBusList() {
                 stopElement.classList.add('suprimida');
                 let suppressedStopAlert = document.createElement('div');
                 suppressedStopAlert.className = 'suppressedStopAlert';
-                suppressedStopAlert.innerHTML = "Parada actualmente suprimida, consulte las alertas de las líneas para más información";
+                suppressedStopAlert.innerHTML = "Parada posiblemente suprimida actualmente, consulte si hay alertas en sus líneas para más información";
                 
                 // Seleccionar el elemento h2 dentro de stopElement
                 const h2Element = stopElement.querySelector('h2');
